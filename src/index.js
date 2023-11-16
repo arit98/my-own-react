9df3fd571af89ed71eebf226a789c4b6da3c60d0
Aritra Das Chowdhury <aritradaschowdhury@gmail.com>
2023-11-16 12:59:11 +0530

    version 2 added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+/* v1
 function reactRender(React, createRoot){
     const domElement = document.createElement(React.type)
     domElement.innerHTML = React.children
     domElement.setAttribute('href', React.props.href)
     domElement.setAttribute('target', React.props.target)
+
+    createRoot.appendChild(domElement)
+}
+*/
+
+// v2
+function reactRender(React, createRoot){
+    const domElement = document.createElement(React.type)
+    domElement.innerHTML = React.children
+    for(const prop in React.props){
+        if(prop === "children") continue;
+        domElement.setAttribute(prop, React.props[prop])
+    }
 
     createRoot.appendChild(domElement)
 }
